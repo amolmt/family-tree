ba910b518b9745bf6b1770873965ea19181e6cf9
Amol Tangade <tangadeamol@gmail.com>
2021-01-24 00:34:59 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import './App.css'
+import OrgChart from '@unicef/react-org-chart'
+import { BrowserRouter, Route } from 'react-router-dom'
+import {
+  tree,
+  tree1,
+  tree2,
+  tree3,
+  tree4,
+  Baliram,
+  Ambadas,
+  Sahadu,
+} from './Tree'
+import avatarPersonnel from './assets/avatar-personnel.svg'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tree: tree,
+      downloadingChart: false,
+      config: {},
+      highlightPostNumbers: [1],
+    }
+  }
+
+  getChild = (id) => {
+    switch (id) {
+      case 100:
+        return tree1
+      case 36:
+        return tree2
+      case 56:
+        return tree3
+      case 25:
+        return tree4
+      case 500:
+        return Baliram
+      case 600:
+        return Ambadas
+      case 700:
+        return Sahadu
+      default:
+        return console.log('no children')
+    }
+  }
+
+  getParent = (d) => {
+    if (d.id === 100) {
+      return {
+        id: 500,
+        person: {
+          id: 500,
+          avatar: avatarPersonnel,
+          department: '',
+          name: 'Pascal ruth',
+          title: 'Member',
+          totalReports: 1,
+        },
+        hasChild: false,
+        hasParent: true,
+        children: [d],
+      }
+    } else if (d.id === 500) {
+      return {
+        id: 1,
+        person: {
+          id: 1,
+          avatar: avatarPersonnel,
+          department: '',
+          name: 'Bryce joe',
+          title: 'Director',
+          totalReports: 1,
+        },
+        hasChild: false,
+        hasParent: false,
+        children: [d],
+      }
+    } else {
+      return d
+    }
+  }
+
+  handleDownload = () => {
+    this.setState({ downloadingChart: false })
+  }
+
+  handleOnChangeConfig = (config) => {
+    this.setState({ config: config })
+  }
+
+  handleLoadConfig = () => {
+    const { config } = this.state
+    return config
+  }
+
+  render() {
+    const { tree, downloadingChart } = this.state
+
+    //For downloading org chart as image or pdf based on id
+    const downloadImageId = 'download-image'
+    const downloadPdfId = 'download-pdf'
+
+    return (
+      <BrowserRouter basename="/react-org-chart">
+        <Route exact path="/">
+          <React.Fragment>
+            <div className="zoom-buttons">
+              <button
+                className="btn btn-outline-primary zoom-button"
+                id="zoom-in"
+              >
+                +
+              </button>
+              <button
+                className="btn btn-outline-primary zoom-button"
+                id="zoom-out"
+              >
+                -
+              </button>
+            </div>
+            <div className="download-buttons">
+              <button className="btn btn-outline-primary" id="download-image">
+                Download as image
+              </button>
+              <button className="btn btn-outline-primary" id="download-pdf">
+                Download as PDF
+              </button>
+
+              {downloadingChart && <div>Downloading chart</div>}
+            </div>
+            <OrgChart
+              tree={tree}
+              downloadImageId={downloadImageId}
+              downloadPdfId={downloadPdfId}
+              onConfigChange={(config) => {
+                this.handleOnChangeConfig(config)
+              }}
+              loadConfig={(d) => {
+                let configuration = this.handleLoadConfig(d)
+                if (configuration) {
+                  return configuration
+                }
+              }}
+              downlowdedOrgChart={(d) => {
+                this.handleDownload()
+              }}
+              loadImage={(d) => {
+                return Promise.resolve(avatarPersonnel)
+              }}
+              loadParent={(d) => {
+                const parentData = this.getParent(d)
+                return parentData
+              }}
+              loadChildren={(d) => {
+                const childrenData = this.getChild(d.id)
+                return childrenData
+              }}
+            />
+          </React.Fragment>
+        </Route>
+      </BrowserRouter>
+    )
+  }
+}
---
+++ src/Tree.js
@@ -0,0 +1,739 @@
+import avatarPersonnel from './assets/avatar-personnel.svg'
+
+export const tree = {
+  id: 100,
+  person: {
+    id: 100,
+    avatar: avatarPersonnel,
+    name: 'ग्यानुबा भानुदास पा तांगडे',
+    title: '',
+    totalReports: 4,
+  },
+  hasChild: true,
+  hasParent: false,
+  children: [],
+}
+
+export const tree1 = [
+  {
+    id: 36,
+    person: {
+      id: 36,
+      avatar: avatarPersonnel,
+      name: 'महादबा ग्यानबा पा तांगडे व पत्नी हरणाबाई ',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 500,
+    person: {
+      id: 500,
+      avatar: avatarPersonnel,
+
+      name: 'बळीराम ग्यानबा पा तांगडे व पत्नी भागीरथीबाई ',
+      title: '',
+      totalReports: 1,
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 700,
+    person: {
+      id: 700,
+      avatar: avatarPersonnel,
+
+      name: 'सहादु ग्यानबा पा तांगडे व पत्नी लक्ष्मीबाई',
+      title: '',
+      totalReports: 9,
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 800,
+    person: {
+      id: 800,
+      avatar: avatarPersonnel,
+
+      name: 'देवराव ग्यानबा पा तांगडे व पत्नी गंगुबाई',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+]
+
+export const tree2 = [
+  {
+    id: 56,
+    person: {
+      id: 56,
+      avatar: avatarPersonnel,
+
+      name: 'Sam John',
+      title: 'HR',
+      totalReports: 2,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 66,
+    person: {
+      id: 66,
+      avatar: avatarPersonnel,
+
+      name: 'John doe',
+      title: 'Developer',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 76,
+    person: {
+      id: 76,
+      avatar: avatarPersonnel,
+
+      name: 'Emilia rogers',
+      title: 'Developer',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 60,
+    person: {
+      id: 60,
+      avatar: avatarPersonnel,
+
+      name: 'Ellen cott',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+]
+
+export const tree3 = [
+  {
+    id: 70,
+    person: {
+      id: 70,
+      avatar: avatarPersonnel,
+
+      name: 'Kenneth dom',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 45,
+    person: {
+      id: 45,
+      avatar: avatarPersonnel,
+
+      name: 'Kin baker',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+]
+
+export const tree4 = [
+  {
+    id: 102,
+    person: {
+      id: 102,
+      avatar: avatarPersonnel,
+
+      name: 'Hendy kinger',
+      title: 'Manager',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 455,
+    person: {
+      id: 455,
+      avatar: avatarPersonnel,
+
+      name: 'Kate baker',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 444,
+    person: {
+      id: 444,
+      avatar: avatarPersonnel,
+
+      name: 'John medis',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+
+  {
+    id: 456,
+    person: {
+      id: 456,
+      avatar: avatarPersonnel,
+
+      name: 'Brett lee',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+]
+
+export const Baliram = [
+  {
+    id: 600,
+    person: {
+      id: 600,
+      avatar: avatarPersonnel,
+
+      name: 'अंबादास बळीराम पा तांगडे व पत्नी यमुनाबाई + सुभद्रा',
+      title: '',
+      totalReports: 7,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+]
+
+export const Ambadas = [
+  {
+    id: 601,
+    person: {
+      id: 601,
+      avatar: avatarPersonnel,
+
+      name: 'लक्ष्मण अंबादास पा तांगडे ',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 602,
+    person: {
+      id: 602,
+      avatar: avatarPersonnel,
+
+      name: 'भगवान अंबादास पा तांगडे ',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 603,
+    person: {
+      id: 603,
+      avatar: avatarPersonnel,
+
+      name: 'रामा अंबादास पा तांगडे',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 604,
+    person: {
+      id: 604,
+      avatar: avatarPersonnel,
+
+      name: 'उत्तम अंबादास पा तांगडे',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 605,
+    person: {
+      id: 605,
+      avatar: avatarPersonnel,
+
+      name: 'अंकुश अंबादास पा तांगडे',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 606,
+    person: {
+      id: 606,
+      avatar: avatarPersonnel,
+
+      name: 'बाबासाहेब अंबादास पा तांगडे',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 607,
+    person: {
+      id: 607,
+      avatar: avatarPersonnel,
+
+      name: 'नारायण अंबादास पा तांगडे ',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+]
+
+export const Sahadu = [
+  {
+    id: 701,
+    person: {
+      id: 701,
+      avatar: avatarPersonnel,
+
+      name: 'किसन सहादु पा तांगडे व पत्नी आसराबाई',
+      title: '',
+      totalReports: 1,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [
+      {
+        id: 7011,
+        person: {
+          id: 7011,
+          avatar: avatarPersonnel,
+
+          name: 'गयाबाई ',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+    ],
+  },
+  {
+    id: 702,
+    person: {
+      id: 702,
+      avatar: avatarPersonnel,
+
+      name: 'श्रीराम सहादु पा तांगडे',
+      title: '',
+      totalReports: 1,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [
+      {
+        id: 7021,
+        person: {
+          id: 7021,
+          avatar: avatarPersonnel,
+
+          name: 'अण्णा श्रीराम पा तांगडे',
+          title: '',
+          totalReports: 2,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [
+          {
+            id: 70211,
+            person: {
+              id: 70211,
+              avatar: avatarPersonnel,
+
+              name: 'लक्ष्मीकांत अण्णा पा तांगडे ',
+              title: '',
+              totalReports: 0,
+            },
+            hasChild: true,
+            hasParent: true,
+            children: [],
+          },
+          {
+            id: 70212,
+            person: {
+              id: 70212,
+              avatar: avatarPersonnel,
+
+              name: 'मयुरी अण्णा पा तांगडे',
+              title: '',
+              totalReports: 0,
+            },
+            hasChild: true,
+            hasParent: true,
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 703,
+    person: {
+      id: 703,
+      avatar: avatarPersonnel,
+
+      name: 'मुरलीधर सहादु पा तांगडे',
+      title: '',
+      totalReports: 4,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [
+      {
+        id: 7031,
+        person: {
+          id: 7031,
+          avatar: avatarPersonnel,
+
+          name: 'किशोर मुरलीधर पा तांगडे',
+          title: '',
+          totalReports: 1,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [
+          {
+            id: 70311,
+            person: {
+              id: 70311,
+              avatar: avatarPersonnel,
+
+              name: 'कन्हैया किशोर पा तांगडे ',
+              title: '',
+              totalReports: 0,
+            },
+            hasChild: true,
+            hasParent: true,
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 7032,
+        person: {
+          id: 7032,
+          avatar: avatarPersonnel,
+
+          name: 'राहुल मुरलीधर पा तांगडे ',
+          title: '',
+          totalReports: 1,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [
+          {
+            id: 70321,
+            person: {
+              id: 70321,
+              avatar: avatarPersonnel,
+
+              name: 'राजवीर राहुल पा तांगडे ',
+              title: '',
+              totalReports: 0,
+            },
+            hasChild: true,
+            hasParent: true,
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 7033,
+        person: {
+          id: 7033,
+          avatar: avatarPersonnel,
+
+          name: 'अयोध्या मुरलीधर पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+      {
+        id: 7034,
+        person: {
+          id: 7034,
+          avatar: avatarPersonnel,
+
+          name: 'पार्वती मुरलीधर पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+    ],
+  },
+
+  {
+    id: 604,
+    person: {
+      id: 604,
+      avatar: avatarPersonnel,
+
+      name: 'मधुकर सहादु पा तांगडे',
+      title: '',
+      totalReports: 1,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [
+      {
+        id: 6041,
+        person: {
+          id: 6041,
+          avatar: avatarPersonnel,
+
+          name: 'अमोल मधुकर पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+    ],
+  },
+
+  {
+    id: 605,
+    person: {
+      id: 605,
+      avatar: avatarPersonnel,
+
+      name: 'सुरेश सहादु पा तांगडे',
+      title: '',
+      totalReports: 3,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [
+      {
+        id: 6051,
+        person: {
+          id: 6051,
+          avatar: avatarPersonnel,
+
+          name: 'अरुण सुरेश पा तांगडे',
+          title: '',
+          totalReports: 1,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [
+          {
+            id: 60511,
+            person: {
+              id: 60511,
+              avatar: avatarPersonnel,
+
+              name: 'कवेश्वर अरुण पा तांगडे',
+              title: '',
+              totalReports: 0,
+            },
+            hasChild: true,
+            hasParent: true,
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 6052,
+        person: {
+          id: 6052,
+          avatar: avatarPersonnel,
+
+          name: 'सतीश सुरेश पा तांगडे ',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+      {
+        id: 6053,
+        person: {
+          id: 6053,
+          avatar: avatarPersonnel,
+
+          name: 'प्रियांका सुरेश पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+    ],
+  },
+
+  {
+    id: 606,
+    person: {
+      id: 606,
+      avatar: avatarPersonnel,
+
+      name: 'महारुद्र सहादु पा तांगडे ',
+      title: '',
+      totalReports: 3,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [
+      {
+        id: 6061,
+        person: {
+          id: 6061,
+          avatar: avatarPersonnel,
+
+          name: 'नितीन महारुद्र पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+      {
+        id: 6062,
+        person: {
+          id: 6062,
+          avatar: avatarPersonnel,
+
+          name: 'दीपाली महारुद्र पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+      {
+        id: 6063,
+        person: {
+          id: 6063,
+          avatar: avatarPersonnel,
+
+          name: 'ज्ञानेश्वरी महारुद्र पा तांगडे',
+          title: '',
+          totalReports: 0,
+        },
+        hasChild: true,
+        hasParent: true,
+        children: [],
+      },
+    ],
+  },
+  {
+    id: 60777,
+    person: {
+      id: 60777,
+      avatar: avatarPersonnel,
+
+      name: 'शालन सहादु पा तांगडे',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 60778,
+    person: {
+      id: 60778,
+      avatar: avatarPersonnel,
+
+      name: 'द्रौपदी सहादु पा तांगडे',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 60779,
+    person: {
+      id: 60779,
+      avatar: avatarPersonnel,
+
+      name: 'तौर',
+      title: '',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+]
